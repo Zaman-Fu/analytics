bf74f5161c05c227b865e9c614d053400832efef
Philip Zamanis <philip.zamanis@solera.com>
2022-10-31 16:43:59 +0100

    spring initializer setup

---
+++ src/test/java/skyviewer/service/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package skyviewer.service.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
